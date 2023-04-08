64be967dfe3cf0b12e17a634b3626b149feeeb3b
raresiacob3 <rares.iacob@student.upt.ro>
2023-04-08 19:07:15 +0300

    Add files via upload
    Added the libraries needed for the project.

---
+++ ProjectOS.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <io.h>
+#include <windows.h>
+
+int main(int argc, char* argv[])
+{
+    printf("Hello World");
+    return 0;
+}
